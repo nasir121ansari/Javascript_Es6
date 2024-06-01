19994cf45cbfc85dc53735dc3963912a8aefdb04
Md Nasir Ansari <nasir121ansari@gmail.com>
2024-06-01 14:21:30 +0530

    changes

--- mutable_and_immutable.js
+++ mutable_and_immutable.js
@@ -29,25 +29,48 @@ let c = {
 // This means that if you modify the object using one variable, the changes will be reflected when you access the object through the other variable. This is due to the fact that objects are reference types in JavaScript.
 
 let d = c; // Mutable, referring to the same memory location
-let z = Object.assign({}, c); // Immutable, referencing a different memory location
-let k = { ...c }; // Immutable, referencing a different memory location
-
 d.city = "Kolkata";
-
 console.log("Original object (c):", c); // { name: "Alice", age: 25, city: "Kolkata" }
 console.log("Modified object (d):", d); // { name: "Alice", age: 25, city: "Kolkata" }
-console.log("Copied object (z):", z); // { name: "Alice", age: 25 }
-console.log("Copied object (k):", k); // { name: "Alice", age: 25 }
+
+let cc = {
+  name: "Alice",
+  age: 25,
+};
+// Immutable, referencing a different memory location
+let z = Object.assign({}, cc);
+z.city = "Patna";
+console.log("Original object (cc):", cc); // { name: "Alice", age: 25,  }
+console.log("Copied object (z):", z); // { name: "Alice", age: 25, city: "Patna" }
+
+let ccc = {
+  name: "Alice",
+  age: 25,
+};
+// Immutable, referencing a different memory location
+let k = { ...ccc };
+k.city = "Patna";
+console.log("Original object (cc):", cc); // { name: "Alice", age: 25,  }
+console.log("Copied object (z):", z); // { name: "Alice", age: 25, city : 'patna' }
 
 // Array: Represents a list of values.
 let a = ["red", "green", "blue"];
-let b = a; // Mutable, referring to the same memory location
-let e = a.concat(); // Immutable, referencing a different memory location
-let f = [...a]; // Immutable, referencing a different memory location
-
-b.pop(); // Modifying the original array through b
-
+let b = a;
+b.pop();
+// Mutable, referring to the same memory location
 console.log("Original array (a):", a); // ["red", "green"]
 console.log("Modified array (b):", b); // ["red", "green"]
-console.log("Copied array (e):", e); // ["red", "green", "blue"]
-console.log("Copied array (f):", f); // ["red", "green", "blue"]
+
+let aa = ["red", "green", "blue"];
+// Immutable, referencing a different memory location
+let e = aa.concat();
+e.pop();
+console.log("Copied array (aa):", aa); // ["red", "green", "blue"]
+console.log("Copied array (e):", e); // ["red", "green"]
+
+let aaa = ["red", "green", "blue"];
+// Immutable, referencing a different memory location
+let f = [...a];
+f.pop();
+console.log("Copied array (aaa):", aaa); // ["red", "green", "blue"]
+console.log("Copied array (f):", f); // ["red", "green",]
