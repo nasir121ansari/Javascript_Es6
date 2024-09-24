ce3aeabb36a0ef781eac710d15fe09018942bab6
Md Nasir Ansari <nasir121ansari@gmail.com>
2024-09-24 15:17:55 +0530

    changes

--- Memoization.js
+++ Memoization.js
@@ -12,27 +12,18 @@ console.log(myMap.get('name'));  // Output: 25
 
 
 function memoize(fn) {
-    // Create a new Map object to store the cached results
+    
     const cache = new Map();
 
     // Return a new function that will be the memoized version of the original function
     return function (...args) {
-        // Generate a unique key for the cache based on the function arguments
         const key = JSON.stringify(args);
-
-        // Check if the result for these arguments is already in the cache
         if (cache.has(key)) {
-            // If it is, return the cached result
             return cache.get(key);
         }
-
-        // If the result is not in the cache, call the original function with the provided arguments
         const result = fn(...args);
-
-        // Store the result in the cache with the generated key
         cache.set(key, result);
 
-        // Return the computed result
         return result;
     };
 }
